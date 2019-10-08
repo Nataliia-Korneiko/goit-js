f048659ea1a4b5b9317790f76a500541eb604210
Nataliia Korneiko <korneyko_ns@ukr.net>
2019-10-08 21:09:19 +0300

    added JS-02

---
+++ JS-02/js/task-3.js
@@ -0,0 +1,28 @@
+"use strict";
+// Задание 3
+// Напиши фукцнию findLongestWord(string), которая принимает параметром произвольную строку (в строке будут только слова и пробелы) и возвращает самое длинное слово в этой строке.
+
+// Вызовы функции для проверки работоспособности твоей реализации.
+
+// console.log(findLongestWord('The quick brown fox jumped over the lazy dog')); // 'jumped'
+
+// console.log(findLongestWord('Google do a roll')); // 'Google'
+
+// console.log(findLongestWord('May the force be with you')); // 'force'
+
+// Решение 3
+const findLongestWord = function(string) {
+  const words = string.split(" ");
+  let maxWordsLength = " ";
+  for (const word of words) {
+    if (word.length > maxWordsLength.length) {
+      maxWordsLength = word;
+    }
+  }
+
+  return maxWordsLength;
+};
+
+console.log(findLongestWord("The quick brown fox jumped over the lazy dog")); // 'jumped'
+console.log(findLongestWord("Google do a roll")); // 'Google'
+console.log(findLongestWord("May the force be with you")); // 'force'
---
+++ JS-02/js/task-4.js
@@ -0,0 +1,53 @@
+"use strict";
+// Задание 4
+// Напиши функцию formatString(string) принимающую строку в параметр string.
+
+// Если длина строки не превышает 40 символов, функция возвращает ее в исходном виде.
+// Если длина больше 40 символов, то функция обрезает строку до 40-ка символов и добавляет в конец строки троеточие '...', после чего возвращает укороченную версию.
+// Вызовы функции для проверки работоспособности твоей реализации.
+
+// console.log(formatString('Curabitur ligula sapien, tincidunt non.'));
+// // вернется оригинальная строка
+
+// console.log(formatString('Vestibulum facilisis, purus nec pulvinar iaculis.'));
+// // вернется форматированная строка
+
+// console.log(formatString('Curabitur ligula sapien.'));
+// // вернется оригинальная строка
+
+// console.log(
+//   formatString(
+//     'Nunc sed turpis. Curabitur a felis in nunc fringilla tristique.',
+//   ),
+// );
+// // вернется форматированная строка
+
+// Решение 4 №1
+function formatString(string) {
+  if (string.length < 40) return string;
+  if (string.length > 40) return `${string.slice(0, 40)} ...`;
+}
+
+console.log(formatString("Curabitur ligula sapien, tincidunt non.")); // вернется оригинальная строка
+console.log(formatString("Vestibulum facilisis, purus nec pulvinar iaculis.")); // вернется форматированная строка
+console.log(formatString("Curabitur ligula sapien.")); // вернется оригинальная строка
+console.log(
+  formatString(
+    "Nunc sed turpis. Curabitur a felis in nunc fringilla tristique."
+  )
+); // вернется форматированная строка
+
+// Решение 4 №2
+// function formatString(string) {
+//   if (string.length > 40) return `${string.slice(0, 40)} ...`;
+//   return string;
+// }
+
+// console.log(formatString("Curabitur ligula sapien, tincidunt non.")); // вернется оригинальная строка
+// console.log(formatString("Vestibulum facilisis, purus nec pulvinar iaculis.")); // вернется форматированная строка
+// console.log(formatString("Curabitur ligula sapien.")); // вернется оригинальная строка
+// console.log(
+//   formatString(
+//     "Nunc sed turpis. Curabitur a felis in nunc fringilla tristique."
+//   )
+// ); // вернется форматированная строка
