ef1fc7ef062cd5ef0e91c085dc464cce5336eaa4
Nataliia Korneiko <korneyko_ns@ukr.net>
2019-10-10 12:45:55 +0300

    changed task-6

--- JS-03/js/task-6.js
+++ JS-03/js/task-6.js
@@ -15,7 +15,7 @@
 
 // console.log(calculateTotalPrice(products, 'Дроид')); // 2800
 
-// Решение 6
+// Решение 6 №1
 const products = [
   { name: "Радар", price: 1300, quantity: 4 },
   { name: "Сканер", price: 2700, quantity: 3 },
@@ -35,3 +35,31 @@ function calculateTotalPrice(arr, productName) {
 
 console.log(calculateTotalPrice(products, "Радар")); // 5200
 console.log(calculateTotalPrice(products, "Дроид")); // 2800
+
+// Решение 6 №2 (Деструктуризация объектов)
+// const products = [
+//   { name: "Радар", price: 1300, quantity: 4 },
+//   { name: "Сканер", price: 2700, quantity: 3 },
+//   { name: "Дроид", price: 400, quantity: 7 },
+//   { name: "Захват", price: 1200, quantity: 2 }
+// ];
+
+// function calculateTotalPrice(arr, productName) {
+//   let totalPrice = 0;
+//   // for (const product of arr) {
+//   //   const { name, price, quantity } = product;
+//   //   if (name === productName) {
+//   //     totalPrice = price * quantity;
+//   //   }
+//   // }
+//   for (const { name, price, quantity } of arr) {
+//     if (name === productName) {
+//       totalPrice = price * quantity;
+//     }
+//   }
+
+//   return totalPrice;
+// }
+
+// console.log(calculateTotalPrice(products, "Радар")); // 5200
+// console.log(calculateTotalPrice(products, "Дроид")); // 2800
